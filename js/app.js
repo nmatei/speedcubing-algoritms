eeaef76444e6010b610698d06cc3992789e59a75
Matei Nicolae <matei.nick@gmail.com>
2016-10-09 15:15:53 +0300

    add PLL algorithms

--- js/app.js
+++ js/app.js
@@ -1,53 +1,133 @@
+var SpeedCubing = {
+    options: {
+        filtered: true
+    },
+    show: {
+        F2L: false,
+        OLL: true,
+        PLL: true
+    },
+    alg: {},
+
+    get: function(id) {
+        return SpeedCubing.algKeys[id];
+    }
+};
 
 $.ajax({
-    url: 'algorithms/oll.json',
-    success: function(algorithms) {
-        var showAlgorithms;
-
-        // TODO temporary add custom filter
-        var selectedAlg = [8,9,10,11,20,22,23,28,29,34,35,42,44,50,51,52,53,54,55,57];
-
-        showAlgorithms = algorithms.filter(function(alg) {
-            return selectedAlg.indexOf(alg.i) !== -1;
+    url: 'algorithms/all.json',
+    success: function (algorithms) {
+        var algKeys = {};
+        algorithms.OLL.forEach(function (el) {
+            el.i = "OLL-" + el.i;
+            algKeys[el.i] = el;
         });
-
-        var ollElements = showAlgorithms.map(function(alg) {
-            var formula = $('<span>' + alg.f + '</span>').text();
-
-            return [
-                '<li class="algorithm" data-alg-id="', alg.i, '" data-toggle="modal" data-target="#algModal" title="', alg.i, ' - ', formula, '">',
-                    '<img src="', alg.s, '">',
-                    // '<div class="f-text">', formula.replace(/\s+/gi, '') ,'</div>',
-                    '<div class="f-text">', alg.f ,'</div>',
-                '</li>'
-            ].join('');
+        algorithms.PLL.forEach(function (el) {
+            el.i = "PLL-" + el.i;
+            algKeys[el.i] = el;
         });
-
-        $('#algorithms').append(ollElements.join(''));
-
-        window.OLL = algorithms;
+        SpeedCubing.alg = algorithms;
+        SpeedCubing.algKeys = algKeys;
+        showAlgorithms();
     }
 });
+
+function showAlgorithms() {
+    var show, algorithms = [];
+
+    $.each(SpeedCubing.show, function(key, visible) {
+        if (visible) {
+            algorithms = algorithms.concat(SpeedCubing.alg[key]);
+        }
+    });
+
+    // TODO temporary add custom filter
+    var filtered = [
+            "OLL-8", "OLL-9", "OLL-10", "OLL-11", "OLL-20", "OLL-22", "OLL-23", "OLL-28", "OLL-29", "OLL-34",
+            "OLL-35", "OLL-42", "OLL-44", "OLL-50", "OLL-51", "OLL-52", "OLL-53", "OLL-54", "OLL-55", "OLL-57",
+            'PLL-E', 'PLL-F', 'PLL-Ra', 'PLL-Rb', 'PLL-Na', 'PLL-Nb', 'PLL-Ga', 'PLL-Gb', 'PLL-Gc', 'PLL-Gd'
+        ],
+        skipAlgs = [
+            28, 55, 57
+        ];
+
+    if (SpeedCubing.options.filtered) {
+        show = algorithms.filter(function (alg) {
+            return filtered.indexOf(alg.i) !== -1;
+        });
+    } else {
+        show = algorithms;
+    }
+
+    var elements = show.map(function (alg) {
+        var formula = $('<span>' + alg.f + '</span>').text();
+
+        return [
+            '<li class="algorithm',
+            skipAlgs.indexOf(alg.i) === -1 ? '' : ' f-skip',
+            '" data-alg-id="', alg.i, '" data-toggle="modal" data-target="#algModal" title="', alg.i, ' - ', formula, '">',
+            '<img src="', alg.s, '">',
+            '<div class="f-text">', alg.f, '</div>',
+            '</li>'
+        ].join('');
+    });
+
+    var $algorithms = $('#algorithms');
+    $algorithms.html('');
+    $algorithms.append(elements.join(''));
+}
 
 $('#algModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget),
         id = button.data('alg-id'),
         modal = $(this);
 
-    var algorithm = OLL[id - 1].f;
+    var algorithm = SpeedCubing.get(id),
+        scramble = $('<span>' + algorithm.f + '</span>').text().replace(/[\(\)]/gi, '').split(/\s+/);
 
-    //algorithm = algorithm.replace(/\s+/gi, '');
+    scramble.reverse();
+    scramble = scramble.map(function (move) {
+        if (move.indexOf('’') !== -1) {
+            move = move.replace('’', '');
+        } else {
+            move += '’';
+        }
+        return move;
+    });
 
-    modal.find('.modal-title').html('Formula: <strong>' + id + '</strong>');
-    modal.find('.modal-body').html(algorithm);
+    modal.find('.modal-title').html('<img class="icon" src="' + algorithm.s +
+        '"><strong>' + id + '</strong>; ' +
+        'Scramble: <span class="scramble">' + scramble.join(' ') + '</span>');
+    modal.find('.modal-body').html(algorithm.f);
 });
 
-
-$('#option-icons').change(function() {
-    $('body').addClass( "f-icons" );
+$('#option-icons').change(function () {
+    $('body').addClass("f-icons");
 });
-$('#option-text').change(function() {
-    $('body').removeClass( "f-icons" );
+$('#option-text').change(function () {
+    $('body').removeClass("f-icons");
+});
+
+$('#option-F2L').change(function () {
+    SpeedCubing.show.F2L = $(this).is(":checked");
+    showAlgorithms();
+});
+$('#option-OLL').change(function () {
+    SpeedCubing.show.OLL = $(this).is(":checked");
+    showAlgorithms();
+});
+$('#option-PLL').change(function () {
+    SpeedCubing.show.PLL = $(this).is(":checked");
+    showAlgorithms();
+});
+
+$('#option-show-all').change(function () {
+    SpeedCubing.options.filtered = false;
+    showAlgorithms();
+});
+$('#option-show-filtered').change(function () {
+    SpeedCubing.options.filtered = true;
+    showAlgorithms();
 });
 
 // TODO show/hide sub-title
--- js/save.js
+++ js/save.js
@@ -16,3 +16,23 @@ $('li.g1-column p').has('img').each(function(i, el) {
     });
 });
 JSON.stringify(olls);
+
+var PLL = [];
+$('li.g1-column p').has('img').each(function(i, el) {
+    el = $(el);
+    var img = el.children('img'),
+        nr = el.children('strong'),
+        formulaEl = el.children('span');
+
+    if(!formulaEl.length) {
+        console.warn('formula not found', el);
+    }
+    console.debug(el, nr.text());
+
+    PLL.push({
+        i: nr.text().replace(/\n/gi, ''),
+        s: img.attr('src').replace('./PLL_files/', './images/pll/'),
+        f: formulaEl.html()
+    });
+});
+JSON.stringify(PLL);
